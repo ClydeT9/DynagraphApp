a6b99c724d802f0bbf7d0876e027042509de158a
Clyde Thomson <thomson.clyde@outlook.com>
2018-08-26 14:50:57 +0200

    Sengrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,17 @@ config.assets.compile = true
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  config.action_mailer.default_url_options = { host: 'https://dynagraph.herokuapp.com/'}
+  config.action_mailer.delivery_method = :smtp
+
+  ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'https://dynagraph.herokuapp.com/',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
 end
